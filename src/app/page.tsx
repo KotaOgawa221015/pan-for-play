95ac231f0b034bb426c63d47279aee6fa5a88357
KotaOgawa221015 <s227007@s.do-johodai.ac.jp>
2026-05-21 15:12:10 +0900

    test: husky and lint-staged check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,14 +44,7 @@ export default async function Page({
           </div>
         </div>
 
-        <div
-          className="absolute ri
-        
-        
-        
-        
-        ght-6 top-1/2 -translate-y-1/2 flex items-center gap-4"
-        >
+        <div className="abs. olute right-6 top-1/2 -translate-y-1/2 flex items-center gap-4">
           <UserMenu />
         </div>
       </header>
