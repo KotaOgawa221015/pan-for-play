82ac538dbbc461a614e3df2ad60c48a1993fdec9
KotaOgawa221015 <s227007@s.do-johodai.ac.jp>
2026-05-25 17:22:32 +0900

    Feat: add migration script to insert default fridges into the database

---
+++ prisma/migrations/20260525081937_add_production_fridges/migration.sql
@@ -0,0 +1,4 @@
+INSERT OR IGNORE INTO "Fridge" ("id", "name", "isDefault", "createdAt", "updatedAt")
+VALUES 
+  ('fridge-16f-fixed-id', '16F', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('fridge-15f-fixed-id', '15F', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
