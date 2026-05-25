da88037b78c49795bddb75bd8ffebd5d1f7bc191
akitorahayashi <hayashi20030703@gmail.com>
2026-05-25 13:50:32 +0900

    feat: reorder components in UploadPage for improved layout

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -57,9 +57,9 @@ export default async function UploadPage({
             </Link>
           </header>
 
-          <UserManagementPanel users={users} currentAdminId={currentAdmin.id} />
-          <FridgeManagementPanel fridges={fridges} />
           <Dashboard recentHistory={recentHistory} fridges={fridges} />
+          <FridgeManagementPanel fridges={fridges} />
+          <UserManagementPanel users={users} currentAdminId={currentAdmin.id} />
           <CleanupPanel />
         </div>
       </div>
